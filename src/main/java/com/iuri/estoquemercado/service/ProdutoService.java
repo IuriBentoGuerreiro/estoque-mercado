b1b2fdcd884bf0ab3a7e306bfcccbf26e1e7fbb8
iuri <bentoiuri@gmail.com>
2024-05-01 10:24:31 -0300

    implementa produtoService

---
+++ src/main/java/com/iuri/estoquemercado/service/ProdutoService.java
@@ -0,0 +1,42 @@
+package com.iuri.estoquemercado.service;
+
+import com.iuri.estoquemercado.dto.ProdutoRequest;
+import com.iuri.estoquemercado.model.Produto;
+import com.iuri.estoquemercado.repository.ProdutoRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.webjars.NotFoundException;
+
+import java.util.List;
+
+@Service
+public class ProdutoService {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    public Produto salvar(ProdutoRequest produtoRequest){
+        return produtoRepository.save(Produto.converterParaProduto(produtoRequest));
+    }
+
+    public Produto pegarPorId(Integer id){
+        return produtoRepository.findById(id).orElseThrow(
+                ()-> new NotFoundException("Recurso não encontrado")
+        );
+    }
+
+    public List<Produto> listar(){
+        return produtoRepository.findAll();
+    }
+
+    public Produto atualizar(Integer id, ProdutoRequest produtoRequest){
+        Produto produtoSalvo = pegarPorId(id);
+        BeanUtils.copyProperties(produtoRequest, produtoSalvo, "id");
+        return produtoRepository.save(produtoSalvo);
+    }
+
+    public void deletar(Integer id){
+        produtoRepository.deleteById(id);
+    }
+}
