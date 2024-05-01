ffa60661f7a705ca50a09c4e15c74e18bf3b1c30
iuri <bentoiuri@gmail.com>
2024-05-01 09:06:30 -0300

    implementa conexao com banco de dados

---
+++ src/main/java/com/iuri/estoquemercado/EstoqueMercadoApplication.java
@@ -0,0 +1,13 @@
+package com.iuri.estoquemercado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoqueMercadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoqueMercadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iuri/estoquemercado/EstoqueMercadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iuri.estoquemercado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueMercadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
