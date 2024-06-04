0537cdf0c86b8d14303a869de90ccb049008b63c
iuri <bentoiuri@gmail.com>
2024-06-04 16:42:43 -0300

    ajustes em metodo de salvar pedido

--- src/main/java/com/iuri/estoquemercado/service/PedidoService.java
+++ src/main/java/com/iuri/estoquemercado/service/PedidoService.java
@@ -4,6 +4,7 @@ import com.iuri.estoquemercado.dto.PedidoRequest;
 import com.iuri.estoquemercado.dto.PedidoResponse;
 import com.iuri.estoquemercado.model.Pedido;
 import com.iuri.estoquemercado.repository.PedidoRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,8 +18,18 @@ public class PedidoService {
     @Autowired
     private PedidoRepository pedidoRepository;
 
+    @Autowired
+    private ProdutoService serviceRepository;
+
+    @Transactional
     public PedidoResponse salvar(PedidoRequest pedidoRequest){
-        return PedidoResponse.converter(pedidoRepository.save(Pedido.converter(pedidoRequest)));
+        var pedido = pedidoRepository.save(Pedido.builder()
+                        .produto(serviceRepository.pegarPorId(pedidoRequest.getIdProduto()))
+                        .quantidade(pedidoRequest.getQuantidade())
+                        .precoTotal(pedidoRequest.getPrecoTotal())
+                        .cliente(pedidoRequest.getCliente())
+                .build());
+        return PedidoResponse.converter(pedido);
     }
 
     public List<PedidoResponse> listar(){
