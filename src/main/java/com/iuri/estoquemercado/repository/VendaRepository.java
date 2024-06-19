79fe034dfcccfb43b841608e85ba6af6959a0935
iuri <bentoiuri@gmail.com>
2024-06-19 13:42:58 -0300

    exclui a entidade venda

--- src/main/java/com/iuri/estoquemercado/repository/VendaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.iuri.estoquemercado.repository;
-
-import com.iuri.estoquemercado.model.Venda;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VendaRepository extends JpaRepository<Venda, Integer> {
-}
